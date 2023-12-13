ef7f0b3221c92091b254a03e9fd39f8e2109b3ba
Praveen G <25690316+cod3cat@users.noreply.github.com>
2023-12-13 23:55:45 +0530

    Made chrome run in headless for github workflow

--- cukes/src/test/java/stepDefinitions/Contact_Us_Test.java
+++ cukes/src/test/java/stepDefinitions/Contact_Us_Test.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
 public class Contact_Us_Test {
@@ -16,6 +17,11 @@ public class Contact_Us_Test {
     private WebDriver driver;
     @Before
     public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
