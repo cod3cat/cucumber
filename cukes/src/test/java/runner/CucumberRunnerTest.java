cf4253936852cbaa1c4e670daa1edc3660426882
Praveen G <25690316+cod3cat@users.noreply.github.com>
2023-12-14 17:32:49 +0530

    Added the ability to run  tests in parallel

--- cukes/src/test/java/runner/CucumberRunnerTest.java
+++ cukes/src/test/java/runner/CucumberRunnerTest.java
@@ -2,8 +2,14 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/resources/features", glue = "stepDefinitions",
-        monochrome = true, plugin = {"pretty", "html:target/cucumber"}, tags = "@regression")
+        monochrome = true, plugin = {"pretty", "html:target/cucumber"}, tags = "@contact-us")
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
