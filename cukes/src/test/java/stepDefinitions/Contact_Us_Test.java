9fcb6fcfc61f5e5f80383b5bfbcf7bef9b59aa11
Praveen G <25690316+cod3cat@users.noreply.github.com>
2023-12-14 21:22:03 +0530

    Implemented a common sendkeys method to wait and send. Updated all step definitions to use the new sendkeys

--- cukes/src/test/java/stepDefinitions/Contact_Us_Test.java
+++ cukes/src/test/java/stepDefinitions/Contact_Us_Test.java
@@ -3,10 +3,8 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import pageObjects.Base_PO;
 
