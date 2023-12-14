a70c7e63c062c458536abbae6129d7a9b3808c90
Praveen G <25690316+cod3cat@users.noreply.github.com>
2023-12-14 17:33:28 +0530

    Added the ability to take screenshots on step failure

--- cukes/src/test/java/stepDefinitions/base/Hooks.java
+++ cukes/src/test/java/stepDefinitions/base/Hooks.java
@@ -1,7 +1,12 @@
 package stepDefinitions.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.messages.types.Timestamp;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static driver.DriverFactory.getDriver;
 import static driver.DriverFactory.cleanupDriver;
@@ -9,6 +14,14 @@ public class Hooks {
     @Before
     public void setup() {
         getDriver();
+    }
+
+    @AfterStep
+    public void takeScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName() + Long.toString(System.currentTimeMillis()));
+        }
     }
 
     @After
